c6da28dee536ebb91db0f3468a78002993195d91
brandonlee781 <brandonlee781@gmail.com>
2016-05-07 21:46:26 -0500

    A lot of changes again. Significantly reduced file size  of images. Removed Normalize.css. Added all the photos to the gallery. Implemented Featherbox as a lightbox.

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,18 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var browserSync = require('browser-sync').create();
 
+var useref = require('gulp-useref');
+var uglify = require('gulp-uglify');
+var gulpIf = require('gulp-if');
+var cssnano = require('gulp-cssnano');
+var imagemin = require('gulp-imagemin');
+var cache = require('gulp-cache');
+var del = require('del');
+
+var runSequence = require('run-sequence');
+
+//-------watch tasks
+
 gulp.task('sass', function(){
   return gulp.src('site/scss/*.scss')
     .pipe(sass()) // Converts Sass to CSS with gulp-sass
@@ -25,3 +37,39 @@ gulp.task('watch', ['browserSync', 'sass'], function (){
   gulp.watch('site/*.html', browserSync.reload);
   gulp.watch('site/js/*.js', browserSync.reload);
 });
+
+//-------- build tasks
+
+gulp.task('useref', function(){
+  return gulp.src('site/*.html')
+    .pipe(useref())
+    .pipe(gulpIf('*.js', uglify()))
+    // Minifies only if it's a CSS file
+    .pipe(gulpIf('*.css', cssnano()))
+    .pipe(gulp.dest('dist'))
+});
+
+gulp.task('images', function(){
+  return gulp.src('site/images/**/*.+(png|jpg|jpeg|gif|svg)')
+  // Caching images that ran through imagemin
+  .pipe(cache(imagemin({
+      interlaced: true
+    })))
+  .pipe(gulp.dest('dist/images'))
+});
+
+gulp.task('clean:dist', function() {
+  return del.sync('dist');
+})
+
+
+//--------- run build tasks
+
+
+
+gulp.task('build', function (callback) {
+  runSequence('clean:dist',
+    ['sass', 'useref', 'images'],
+    callback
+  )
+})
